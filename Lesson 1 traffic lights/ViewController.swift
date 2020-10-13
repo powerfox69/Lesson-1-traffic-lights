aceb1949aa6c3afe9920518a5d8448505b7a677b
Gleb <gleb.yandex.ru>
2020-10-14 00:05:22 +0300

    one

--- Lesson 1 traffic lights/ViewController.swift
+++ Lesson 1 traffic lights/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redColorView: UIView!
+    @IBOutlet var yellowColorView: UIView!
+    @IBOutlet var greenColorView: UIView!
+    
+    @IBOutlet var toggleTextButton: UIButton!
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redColorView.alpha = 0.3
+        redColorView.layer.cornerRadius = 88
+        yellowColorView.alpha = 0.3
+        yellowColorView.layer.cornerRadius = 88
+        greenColorView.alpha = 0.3
+        greenColorView.layer.cornerRadius = 88
+        toggleTextButton.layer.cornerRadius = 10
     }
 
-
+    @IBAction func toggelTextButtonPressed() {
+        if redColorView.alpha < 0 {
+            redColorView.alpha = 1
+        }
+            toggleTextButton.setTitle("Next", for: .normal)
+        
+        }
 }
+    
+
+
 
